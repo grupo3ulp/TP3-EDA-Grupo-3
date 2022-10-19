665d7f5520affde157f044ba5e583d1f8230b05e
Ni-luteno <110200618+Ni-luteno@users.noreply.github.com>
2022-10-19 18:07:53 -0300

    Update G3Ejercicio4.cpp

--- G3Ejercicio4.cpp
+++ G3Ejercicio4.cpp
@@ -25,6 +25,12 @@ int main() {
     Fecha f2 = {22, 7, 1998};
     Persona p2 = {"Esteban", 19, 60, f2};
     Empleado e = {p2, 50000};
+    
+    cout<<"Persona 1:"<<endl;
+    cout<<"Nombre: "<<p1.nombre<<"\nEdad: "<<p1.edad<<"\npeso: "<<p1.peso<< "\nFecha nacimiento: "<< p1.fecha.dia<<"/"<<p1.fecha.mes<<"/"<<p1.fecha.anio<<endl;
+    cout<<"-------------------------------------------------------"<<endl;
+    cout<<"Empleado 1:"<<endl;
+    cout<<"Nombre: "<<e.persona.nombre<<"\nEdad: "<<e.persona.edad<<"\npeso: "<<e.persona.peso<< "\nFecha nacimiento: "<< e.persona.fecha.dia<<"/"<<e.persona.fecha.mes<<"/"<<e.persona.fecha.anio<<"\nSalario: "<<e.salario<<endl;
 
     return 0;
 }
