9f8d9bd3fbb11f2862549753abc25c0e7167f794
Marco Silva <nelsonmarcosilva@gmail.com>
2022-10-19 23:12:17 -0300

    variable global en el 1

--- G3Ejercicio1.cpp
+++ G3Ejercicio1.cpp
@@ -10,35 +10,40 @@ letras. Luego compare si dos títulos son del mismo artista.*/
 
 using namespace std;
 
-typedef struct discoCompacto{
+typedef struct discoCompacto
+{
     string titulo;
     const char *artista;
     int canciones;
     double precio;
     int lanzamiento;
-}CD;
+} CD;
 
-int main() {
-    CD cd1 = {"Relapse", "Eminem", 20, 15000, 2009};
-    CD cd2 = {"Night Visions", "ImagineDragons", 12, 12000, 2012};
+CD cd1 = {"Relapse", "Eminem", 20, 15000, 2009};
+CD cd2 = {"Night Visions", "ImagineDragons", 12, 12000, 2012};
+
+int main()
+{
+
     CD cd3 = {"Evolve", "ImagineDragons", 11, 18000, 2017};
 
-    CD cds[3] = {cd1,cd2,cd3};
+    CD cds[3] = {cd1, cd2, cd3};
 
-    cout<<"Los artistas con mas de 10 letras en el nombre son:"<<endl;
-    cout<<"---------------------------------------------------"<<endl;
-    for (int i = 0; i < 3; ++i) {
-        if(strlen(cds[i].artista)>10){
-            cout <<cds[i].artista<< endl;
+    cout << "Los artistas con mas de 10 letras en el nombre son:" << endl;
+    cout << "---------------------------------------------------" << endl;
+    for (int i = 0; i < 3; ++i)
+    {
+        if (strlen(cds[i].artista) > 10)
+        {
+            cout << cds[i].artista << endl;
         }
     }
-    cout<<"---------------------------------------------------"<<endl;
+    cout << "---------------------------------------------------" << endl;
 
-    if (cds[0].artista == cds[1].artista || cds[0].artista == cds[2].artista || cds[1].artista == cds[2].artista){
-        cout<<"Hay por lo menos 2 cds del mismo artista"<<endl;
+    if (cds[0].artista == cds[1].artista || cds[0].artista == cds[2].artista || cds[1].artista == cds[2].artista)
+    {
+        cout << "Hay por lo menos 2 cds del mismo artista" << endl;
     }
 
     return 0;
 }
-
- 
